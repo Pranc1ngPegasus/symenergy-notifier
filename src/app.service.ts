192e373e1a8173c2bb39a32a552f39e321b535ec
Pranc1ngPegasus <ride.or.die.2215@icloud.com>
2022-07-08 00:51:53 +0900

    implement server endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { SymenergyScanner, ScanResult } from './scanner/index';
+import { ApiConfigService } from 'src/config/config.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly config: ApiConfigService) {}
+
   getHealthcheck(): string {
     return 'ok';
+  }
+
+  async scan(): Promise<ScanResult> {
+    const auth = {
+      id: this.config.loginId,
+      password: this.config.loginPassword,
+      contractId: this.config.contractId,
+    };
+    const scanner = new SymenergyScanner(auth);
+
+    return await scanner.scan();
   }
 }
